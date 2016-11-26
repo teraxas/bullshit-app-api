87ad23b637b7303b26c76fb3a2a43e7e2a53c092
Karolis Jocevičius <Karolis Joceviius>
2016-11-26 17:48:25 +0200

    no message

--- bullshit-app/src/main/resources/static/js/hello.js
+++ bullshit-app/src/main/resources/static/js/hello.js
@@ -1,26 +1,21 @@
-var bullshitBegan = true;
-//var showApp = angular.module('showApp', [])
 
 angular.module('hello', []).controller('home', function($http) {
 	var self = this;
+	
+	// set the default value of our number
+	$http.myNumber = 0;
+	
+	// function to evaluate if a number is even
+	$http.isEven = function(value) {
+
+		if (value % 2 == 0)
+			return true;
+		else
+			return false;
+
+	};
+	
 	$http.get('question/resource/').then(function(response) {
 		self.greeting = response.data;
 	})
 });
-
-//.controller('mainController', function($scope) {
-//	  
-//	  // set the default value of our number
-//	  $scope.myNumber = 0;
-//	        
-//	  // function to evaluate if a number is even
-//	  $scope.isEven = function(value) {
-//	    
-//	    if (value % 2 == 0)
-//	      return true;
-//	    else 
-//	      return false;
-//	    
-//	  };
-//	  
-//	});
