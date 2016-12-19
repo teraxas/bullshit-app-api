45402852e419fd6a3764a9ca118eb2bf2ad0d93e
Karolis Jocevičius <karolis.jocevicius@gmail.com>
2016-12-19 19:06:20 +0200

    JDBC_DATABASE_URL log

--- src/main/java/lt/mesgalis/bullshit/Application.java
+++ src/main/java/lt/mesgalis/bullshit/Application.java
@@ -22,7 +22,9 @@ public class Application {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+
         printLoadedBeans(ctx);
+        log.info("JDBC_DATABASE_URL: " + System.getenv("JDBC_DATABASE_URL"));
     }
 
     @Bean
