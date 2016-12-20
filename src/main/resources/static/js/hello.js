239884f391d46563199dcd67d3ccb7f5005a6409
Karolis Jocevičius <karolis.jocevicius@gmail.com>
2016-12-20 08:19:17 +0200

    CSS loader spinner on http requests

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,18 +1,23 @@
 
 angular.module('bullshit', []).controller('question', function($http, $scope) {
+	$scope.loading = true;
 	var self = this;
 
 	$scope.loadQuestion = function() {
+		$scope.loading = true;
 		$http.get('question/get/').then(function(response) {
 			$scope.question = response.data;
 			$scope.lastResponseResult = null;
 			$rootScope.$$phase || $rootScope.$apply();
 
 			console.log("Question received: " + $scope.question);
+		}).finally(function() {
+			$scope.loading = false;
 		});
 	};
 
 	$scope.answer = function(isBullshit) {
+		$scope.loading = true;
 		$http.post('question/answer/', {
 			id: $scope.question.id,
 			answer: isBullshit
@@ -20,6 +25,8 @@ angular.module('bullshit', []).controller('question', function($http, $scope) {
 			$scope.lastResponseResult = response.data;
 
 			console.log("Answer received: ", $scope.lastResponseResult);
+		}).finally(function() {
+			$scope.loading = false;
 		});
 	};
 
